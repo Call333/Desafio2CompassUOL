779e76ff5ddaecb637cdb3b948d725b51d0f98ed
Call333 <cam4@aluno.ifal.edu.br>
2024-07-09 11:54:55 -0300

    add mudanças na formad e add nas listas

--- src/app/App.java
+++ src/app/App.java
@@ -5,26 +5,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.entities.Alimento;
+import model.entities.Centro;
 import model.entities.ItemEstoque;
 import model.entities.ProdutoHigiene;
 import model.entities.Roupa;
 
 public class App {
     public static void main(String[] args) {
-        List<ItemEstoque> itens = new ArrayList<>();
+        Centro centro01 = new Centro("Centro de Distribuição Esperança", "Av. Boqueirão, 2450 - Igara, Canoas - RS", "92032-420");
+        
+        Alimento i1 = new Alimento("Arroz", 64, 3, "5Kg", LocalDate.now());
+        centro01.addAlimento(i1);
+        Alimento l1 = new Alimento("Arroz", 40, 3, "1Kg", LocalDate.now());
+        centro01.addAlimento(l1);
+        Roupa i2 = new Roupa("Agasalho", 3, 1, "F", "G");
+        centro01.addRoupa(i2);
+        // Roupa i3 = new Roupa("Agasalho", 3, 1, "F", "G");
+        // centro01.addRoupa(i3);
+        // ProdutoHigiene i4 = new ProdutoHigiene("Escova Oral-B", 10, 1, "Escova");
+        // centro01.addHigiene(i4);
 
-        ItemEstoque i1 = new Alimento("Arroz", 12, 3, "Kg", LocalDate.now());
-        itens.add(i1);
-        i1 = new Roupa("Agasalho", 3, 1, "F", "G");
-        itens.add(i1);
-        i1 = new ProdutoHigiene("Escova Oral-B", 10, 1, "Escova");
-        itens.add(i1);
-
-        int qtdSoma = 0;
-        for (ItemEstoque itemEstoque : itens) {
-            qtdSoma += itemEstoque.getQuantidade();
+        int qtdTotal = 0;
+        for (ItemEstoque itemEstoque : centro01.getEstoque()) {
+            // qtdTotal += itemEstoque.getQuantidade();
             System.out.println(itemEstoque);
         }
-        System.out.println("QtdTotalDeItens = " + qtdSoma);
+        // System.out.println(centro01.getEstoqueTotal());
+        // System.out.println(centro01.getEstoqueAlimentos());
+        // centro01.getEstoqueAlimentos().remove(1);
+        System.out.println("QtdTotalDeItensDeAlimento = " + centro01.contabilizarTotalItensAlimento());
+        
+        System.out.println("QtdTotalDeItens = " + centro01.contabilizarTotalItens());
     }
 }   
--- src/model/entities/Centro.java
+++ src/model/entities/Centro.java
@@ -1,5 +1,201 @@
 package model.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Centro {
-    
+    private String nome;
+    private String endereco;
+    private String cep;
+
+    private List<ItemEstoque> estoqueTotal = new ArrayList<>();
+
+    private List<Roupa> estoqueRoupas = new ArrayList<>();
+    private List<Alimento> estoqueAlimentos = new ArrayList<>();
+    private List<ProdutoHigiene> estoqueHigiene = new ArrayList<>();
+
+    public Centro(String nome, String endereco, String cep) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cep = cep;
+    }
+
+    public void addRoupa(Roupa i) {
+        estoqueRoupas.add(i);
+        int qi = i.getQuantidade();
+        int h = contabilizarTotalItensRoupa();
+        Roupa n1 = null;
+        if (h > 1000) {
+            for (int j = 0; j < (contabilizarTotalItensRoupa() - 1000); j++) {
+                qi -= 1;
+                n1 = new Roupa(i.getDescricao(), qi,
+                        i.getCentro_distruibuicao_id(), i.getGenero(), i.getTamanho());
+            }
+            estoqueRoupas.remove(i);
+            estoqueRoupas.add(n1);
+            System.out.println("Agora o estoque de roupas do centro de distribuição está cheio.");
+        }
+        if (n1 != null) {
+            estoqueTotal.add(n1);
+        } else {
+            estoqueTotal.add(i);
+        }
+    }
+
+    // public void addAlimento(Alimento i) { /// Alimento, contabilizar itens me retorne 999 + 5 = 1000, tira 4
+    //     estoqueAlimentos.add(i);
+    //     int qi = i.getQuantidade();
+    //     int h = contabilizarTotalItensAlimento();
+    //     Alimento n1 = null;
+    //     if (h > 1000) {
+    //         for (int j = 0; j < (contabilizarTotalItensAlimento() - 1000); j++) {
+    //             qi -= 1;
+    //             n1 = new Alimento(i.getDescricao(), qi,
+    //                     i.getCentro_distruibuicao_id(), i.getUnidade_medida(), i.getValidade());
+    //         }
+    //         estoqueAlimentos.remove(i);
+    //         estoqueAlimentos.add(n1);
+    //         System.out.println("Agora o estoque de alimentos do centro de distribuição está cheio.");
+    //     }
+    //     if (n1 != null) {
+    //         estoqueTotal.add(n1);
+    //     } else {
+    //         estoqueTotal.add(i);
+    //     }
+    // }
+
+    public void addAlimento(Alimento i) { /// Alimento, contabilizar itens me retorne 999 + 5 = 1000, tira 4
+        int qtd = i.getQuantidade();
+        for (int j = 0; j < qtd; j++) { // j representa a quantidade de arroz q será add no estoque.
+            estoqueAlimentos.add(new Alimento(i.getDescricao(), 1, 
+            i.getCentro_distruibuicao_id(), i.getUnidade_medida(), i.getValidade()));
+            estoqueTotal.add(new Alimento(i.getDescricao(), 1, 
+            i.getCentro_distruibuicao_id(), i.getUnidade_medida(), i.getValidade()));
+        }
+        System.out.println(estoqueAlimentos.size());
+        int limite = 100; // Limite tem q estar em 100
+        if(estoqueAlimentos.size()> limite){ // 10
+            while (estoqueAlimentos.size() > limite) {
+                estoqueAlimentos.remove(estoqueAlimentos.size() - 1);
+                estoqueTotal.remove(estoqueTotal.size() - 1);
+            }
+        }
+    }
+
+    public void addHigiene(ProdutoHigiene i) {
+        estoqueHigiene.add(i);
+        int qi = i.getQuantidade();
+        int h = contabilizarTotalItensHigiene();
+        ProdutoHigiene n1 = null;
+        if (h > 1000) {
+            for (int j = 0; j < (contabilizarTotalItensHigiene() - 1000); j++) {
+                qi -= 1;
+                n1 = new ProdutoHigiene(i.getDescricao(), qi,
+                        i.getCentro_distruibuicao_id(), i.getTipo());
+            }
+            estoqueHigiene.remove(i);
+            estoqueHigiene.add(n1);
+            System.out.println("Agora o estoque de produtos de higiene centro de distribuição está cheio.");
+        }
+        if (n1 != null) {
+            estoqueTotal.add(n1);
+        } else {
+            estoqueTotal.add(i);
+        }
+    }
+
+    public void remove(Roupa i) {
+        estoqueRoupas.remove(i);
+        estoqueTotal.remove(i);
+    }
+
+    public void remove(Alimento i) {
+        estoqueAlimentos.remove(i);
+        estoqueTotal.remove(i);
+    }
+
+    public void remove(ProdutoHigiene i) {
+        estoqueHigiene.remove(i);
+        estoqueTotal.remove(i);
+    }
+
+    public int contabilizarTotalItens() {
+        int qtdTotal = 0;
+        for (ItemEstoque itemEstoque : getEstoqueTotal()) {
+            qtdTotal += itemEstoque.getQuantidade();
+        }
+        return qtdTotal;
+    }
+
+    public int contabilizarTotalItensHigiene() {
+        int qtdTotal = 0;
+        for (ProdutoHigiene itemEstoque : getEstoqueHigiene()) {
+            qtdTotal += itemEstoque.getQuantidade();
+        }
+        return qtdTotal;
+    }
+
+    public int contabilizarTotalItensAlimento() {
+        int qtdTotal = 0;
+        for (Alimento itemEstoque : getEstoqueAlimentos()) {
+            qtdTotal += itemEstoque.getQuantidade();
+        }
+        return qtdTotal;
+    }
+
+    public int contabilizarTotalItensRoupa() {
+        int qtdTotal = 0;
+        for (Roupa itemEstoque : getEstoqueRoupas()) {
+            qtdTotal += itemEstoque.getQuantidade();
+        }
+        return qtdTotal;
+    }
+
+    public List<ItemEstoque> getEstoque() {
+        return estoqueTotal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public List<ItemEstoque> getEstoqueTotal() {
+        return estoqueTotal;
+    }
+
+    public List<Roupa> getEstoqueRoupas() {
+        return estoqueRoupas;
+    }
+
+    public List<Alimento> getEstoqueAlimentos() {
+        return estoqueAlimentos;
+    }
+
+    public List<ProdutoHigiene> getEstoqueHigiene() {
+        return estoqueHigiene;
+    }
+
+    public String toString() {
+        return "Centro -> [ Nome: " + nome + ", " + endereco + ", " + cep + " ]";
+    }
 }
